48e024f92346e2c182302e1c4b379e9e612a0825
qAbdullajon <azikmelor7705@gmail.com>
2025-06-06 01:42:46 +0500

    ewewe

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
-// server.js
-const express = require("express");
-const cors = require("cors");
 require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
 
 const authRouter = require("./routes/auth.routes");
 
-const app = express();
+function starter() {
+  try {
+    const app = express();
+    const PORT = process.env.PORT;
 
-app.use(
-  cors({
-    origin: "https://hrms-neon-xi.vercel.app",
-    credentials: true,
-  })
-);
+    app.use(
+      cors({
+        origin: "https://hrms-neon-xi.vercel.app",
+        credentials: true,
+      })
+    );
+    app.use(express.json());
+    app.use("/api/auth", authRouter);
 
-app.use(express.json());
-app.use("/api/auth", authRouter);
+    app.listen(PORT, console.log(`server is running on ${PORT} port!`));
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-module.exports = app;
+starter();
