f0ff32fcf3c3a34b76fcfc12976478dd7dfb7605
qAbdullajon <azikmelor7705@gmail.com>
2025-06-06 00:57:16 +0500

    ewwe

--- config/db.js
+++ config/db.js
@@ -5,6 +5,12 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   host: process.env.DB_HOST,
   dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // bu NileDB uchun kerak bo'lishi mumkin
+    }
+  }
 });
 
 module.exports = sequelize;
