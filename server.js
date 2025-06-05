09a435aab8563a0cd416368a0e4debef793c2845
qAbdullajon <azikmelor7705@gmail.com>
2025-06-06 01:22:15 +0500

    ewewe

--- server.js
+++ server.js
@@ -4,24 +4,16 @@ const express = require("express");
 
 const authRouter = require("./routes/auth.routes");
 
-function starter() {
-  try {
-    const app = express();
-    const PORT = process.env.PORT;
+const app = express();
 
-    app.use(
-      cors({
-        origin: "https://hrms-neon-xi.vercel.app",
-        credentials: true,
-      })
-    );
-    app.use(express.json());
-    app.use("/api/auth", authRouter);
+app.use(
+  cors({
+    origin: "https://hrms-neon-xi.vercel.app",
+    credentials: true,
+  })
+);
 
-    app.listen(PORT, console.log(`server is running on ${PORT} port!`));
-  } catch (error) {
-    console.log(error);
-  }
-}
+app.use(express.json());
+app.use("/api/auth", authRouter);
 
-starter();
+module.exports = app;
